7bae3aba7663ac4a2ddab58410f270c7031dda53
carmeljane <carmelbarwick@gmail.com>
2024-05-04 14:17:19 +0100

    basic styling for quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 function Quiz({ question, handleAnswerOptionClick }) {
   return (
-    <div>
-      <div>{question.questionText}</div>
+    <div className="card bg-gray-100 border border-gray-300 p-6 rounded-lg shadow-md">
+      <div className="text-2xl mb-4 text-gray-800">{question.questionText}</div>
       {question.answerOptions.map((option, index) => (
-        <button onClick={() => handleAnswerOptionClick(option.house)} key={index}>
+        <button
+          className={`bg-${option.house.toLowerCase()}-400 hover:bg-${option.house.toLowerCase()}-500 text-gray-900 font-bold py-2 px-4 rounded-md inline-flex items-center mb-4`}
+          onClick={() => handleAnswerOptionClick(option.house)}
+          key={index}
+        >
           {option.answerText}
         </button>
       ))}
