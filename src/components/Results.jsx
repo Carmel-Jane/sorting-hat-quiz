3848443f45386f30361e28f2e55fdf60dba26051
carmeljane <carmelbarwick@gmail.com>
2024-05-04 12:16:19 +0100

    results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Results = () => {
-  return (
-    <div>Results</div>
-  )
-}
 
-export default Results
+function Result({ scores }) {
+    const house = Object.keys(scores).reduce((a, b) => scores[a] > scores[b] ? a : b);
+  
+    return (
+      <div>
+        <h2>Your Hogwarts house is {house}!</h2>
+      </div>
+    );
+  }
+  
+  export default Result;
