08e9753d86440fa5e001edcce6107e729267d010
carmeljane <carmelbarwick@gmail.com>
2024-05-04 12:13:01 +0100

    set up state in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import questions from './questions';
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [scores, setScores] = useState({ Gryffindor: 0, Hufflepuff: 0, Ravenclaw: 0, Slytherin: 0 });
+  const [showResult, setShowResult] = useState(false);
+
   return (
     <div className="App">
       Hello, world!
