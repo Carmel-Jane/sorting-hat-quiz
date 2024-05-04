0508ad087949262e634bdb91c4df39fe804dd777
carmeljane <carmelbarwick@gmail.com>
2024-05-04 12:10:33 +0100

    some questions added to questions file

--- src/questions.js
+++ src/questions.js
@@ -7,4 +7,22 @@ const questions = [
       { answerText: 'Facebook', house: 'Hufflepuff' },
       { answerText: 'LinkedIn', house: 'Slytherin' },
     ],
-  }]
+  },
+  {
+    questionText: 'What type of vacation do you prefer?',
+    answerOptions: [
+      { answerText: 'Adventure-filled hiking trip', house: 'Gryffindor' },
+      { answerText: 'Cultural exploration in a foreign city', house: 'Ravenclaw' },
+      { answerText: 'Relaxing beach getaway', house: 'Hufflepuff' },
+      { answerText: 'Luxurious resort stay', house: 'Slytherin' },
+    ],
+  },
+  {
+    questionText: 'How do you spend your free time?',
+    answerOptions: [
+      { answerText: 'Outdoor activities', house: 'Gryffindor' },
+      { answerText: 'Reading or learning something new', house: 'Ravenclaw' },
+      { answerText: 'Spending time with friends and family', house: 'Hufflepuff' },
+      { answerText: 'Working on personal projects', house: 'Slytherin' },
+    ],
+  },]
