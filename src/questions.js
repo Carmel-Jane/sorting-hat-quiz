5db1c585c8df10321deffdcdc2d2e7cc3d1b3339
carmeljane <carmelbarwick@gmail.com>
2024-05-04 11:49:02 +0100

    created questions file, written first question object

---
+++ src/questions.js
@@ -0,0 +1,10 @@
+const questions = [
+{
+    questionText: 'What is your preferred social media platform?',
+    answerOptions: [
+      { answerText: 'Instagram', house: 'Gryffindor' },
+      { answerText: 'Twitter', house: 'Ravenclaw' },
+      { answerText: 'Facebook', house: 'Hufflepuff' },
+      { answerText: 'LinkedIn', house: 'Slytherin' },
+    ],
+  }]
